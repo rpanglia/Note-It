e88eec13b3536282b79c8f141b8095ee50dadeec
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-25 12:31:32 -0500

    html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,13 @@
-const router = require("express").Router();
+const express = require('express');
+const path = require('path');
+
+
+const htmlRouters = require('express').Router();
+
+
+htmlRouters.get('/notes', (req,res) => {res.sendFile(path.join(__dirname, '../public/notes.html'))});
+htmlRouters.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../public/notes.html'))});
+
+htmlRouters.get('*', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'))});
+
+module.exports = htmlRouters;
