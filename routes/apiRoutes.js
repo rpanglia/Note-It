3ded248d46a699c8701195fccf00d6b614af5686
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-25 14:48:15 -0500

    delete added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -42,7 +42,7 @@ apiRouters.post('/api/notes', (req, res) => {
     //     text: req.body.text
     // };
 
-    let newNote = req.body;
+    let newNote = req.body; //can be reassigned
     newNote['id'] = Date.now();
     newNote['title'] = req.body.title;
     newNote['text'] = req.body.text;
@@ -64,18 +64,39 @@ apiRouters.post('/api/notes', (req, res) => {
     });
 
 });
-
-
 //DELETE to be added here
 apiRouters.delete('/api/notes/:id', (req, res) => {
-    const removeNote = req.params.id;
 
-    notes = notes.filter( (note) => {
-        return note.id !== removeNote;
+    fs.readFile('./db/db.json', 'utf8', (err, file) => {
+        if (err) throw err;
+
+        let removeNote = req.params.id; //can be reassigned
+
+        const addNotes = JSON.parse(file);
+        const combinedNotes = addNotes.filter(elem => elem.id !== removeNote);
+
+        const allNotes = JSON.stringify(combinedNotes);
+        fs.writeFile('./db/db.json', allNotes, 'utf8', (err) => {
+            if (err) throw err;
+            console.log("Note deleted successfully");
+        });
+        
+        return res.send(JSON.parse(allNotes));
+
+
+
     });
 
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
-    res.send("Note has been deleted.");
+    // const removeNote = req.params.id;
+
+    // notes = notes.filter( (note) => {
+    //     return note.id !== removeNote;
+    // });
+
+    // fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+    // res.send("Note has been deleted.");
+
+
 });
 
 
