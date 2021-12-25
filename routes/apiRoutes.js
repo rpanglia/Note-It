2d5c9248ee5dcb266691bf5f326c9ca3a4f1f412
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-25 14:04:37 -0500

    api route testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const db = require('./../db/db.json');
+const { uuid } = require('uuidv4');
 
-const apiRouters = require('express').Router();
+const apiRouters = express.Router();
+// let notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
 
 //GET
 apiRouters.get('/api/notes', (req, res) => {
@@ -18,14 +21,23 @@ apiRouters.get('/api/notes', (req, res) => {
 //POST
 apiRouters.post('/api/notes', (req, res) => {
 
-    let noteId = uuid();
-    let newNote = {
-        id: noteId,
-        title: req.body.title,
-        text: req.body.text
-    };
+    // const newNote = req.body;
+    // newNote.id = uuidv4();
+    // notes = 
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    // let noteId = uuid();
+    // let newNote = {
+    //     id: noteId,
+    //     title: req.body.title,
+    //     text: req.body.text
+    // };
+
+    let newNote = req.body;
+    newNote['id'] = Date.now();
+    newNote['title'] = req.body.title;
+    newNote['text'] = req.body.text;
+
+    fs.readFile('./db/db.json', 'utf8', (err, file) => {
         if (err) throw err;
 
         const addNotes = JSON.parse(file);
