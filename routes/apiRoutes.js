491f7437fa56bb8dff6d5c57b038442c0b5710ae
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-25 14:24:04 -0500

    updating apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const db = require('./../db/db.json');
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
 const apiRouters = express.Router();
-// let notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+
+let notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+
 
 //GET
 apiRouters.get('/api/notes', (req, res) => {
@@ -18,12 +20,20 @@ apiRouters.get('/api/notes', (req, res) => {
     });
 });
 
+
 //POST
 apiRouters.post('/api/notes', (req, res) => {
 
-    // const newNote = req.body;
-    // newNote.id = uuidv4();
-    // notes = 
+    const newNote = req.body;
+    newNote.id = uuidv4();
+
+    notes.push(newNote);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+    res.json(notes);
+
+
+
 
     // let noteId = uuid();
     // let newNote = {
@@ -32,32 +42,41 @@ apiRouters.post('/api/notes', (req, res) => {
     //     text: req.body.text
     // };
 
-    let newNote = req.body;
-    newNote['id'] = Date.now();
-    newNote['title'] = req.body.title;
-    newNote['text'] = req.body.text;
+    // let newNote = req.body;
+    // newNote['id'] = Date.now();
+    // newNote['title'] = req.body.title;
+    // newNote['text'] = req.body.text;
 
-    fs.readFile('./db/db.json', 'utf8', (err, file) => {
-        if (err) throw err;
+    // fs.readFile('./db/db.json', 'utf8', (err, file) => {
+    //     if (err) throw err;
 
-        const addNotes = JSON.parse(file);
-        addNotes.push(newNote);
+    //     const addNotes = JSON.parse(file);
+    //     addNotes.push(newNote);
 
-        const allNotes = JSON.stringify(addNotes);
+    //     const allNotes = JSON.stringify(addNotes);
 
-        fs.writeFile('./db/db.json', allNotes, 'utf8', (err) => {
-            if (err) throw err;
-            console.log("Added new note successfully");
-        });
+    //     fs.writeFile('./db/db.json', allNotes, 'utf8', (err) => {
+    //         if (err) throw err;
+    //         console.log("Added new note successfully");
+    //     });
 
-        return res.send(JSON.parse(allNotes));
-    });
+    //     return res.send(JSON.parse(allNotes));
+    // });
 
 });
 
 
 //DELETE to be added here
+apiRouters.delete('/api/notes/:id', (req, res) => {
+    const removeNote = req.params.id;
 
+    notes = notes.filter( (note) => {
+        return note.id !== removeNote;
+    });
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+    res.send("Note has been deleted.");
+});
 
 
 module.exports = apiRouters;
